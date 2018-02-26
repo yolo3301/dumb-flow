43c713c2ab411220c8b231218afd631fba8557c1
ducs1988 <ducs1988@gmail.com>
2018-02-26 00:15:56 -0600

    Define EventType / EventState, assign const values to these type defs

--- pkg/df-model/event.go
+++ pkg/df-model/event.go
@@ -5,8 +5,27 @@ import (
 	"time"
 )
 
+// EventType defines the type of current event ,this type will be assigned when event created
+type EventType string
+
+const (
+	start    EventType = "Start"
+	timeout  EventType = "Timeout"
+	complete EventType = "Complete"
+)
+
+// EventState defines current status of this event
+type EventState string
+
+const (
+	running     EventState = "Running"
+	pending     EventState = "Pending"
+	unavailable EventState = "Unavailable"
+)
+
 // Event defines the model of a workflow event.
 type Event struct {
+<<<<<<< Updated upstream
 	EventID        string
 	WorkflowName   string
 	WorkflowExecID string
@@ -15,6 +34,48 @@ type Event struct {
 	Payload        string
 	CreateTime     time.Time
 	CompleteTime   time.Time
+||||||| merged common ancestors
+	workflowName   string
+	workflowExecID string
+	workItemName   string
+	workItemExecID string
+	eventType      string    // work start / timeout / complete (assigned when event created) 
+	state          string    // running / pending / ... (changable)
+	payload        string    // metadata - 
+	createTime     time.Time 
+	completeTime   time.Time
+=======
+<<<<<<< HEAD
+	workflowName   string
+	workflowExecID string
+	workItemName   string
+	workItemExecID string
+	eventType      EventType  // work start / timeout / complete (assigned when event created)
+	state          EventState // running / pending / ... (changable)
+	payload        string     // metadata -
+	createTime     time.Time
+	completeTime   time.Time
+||||||| merged common ancestors
+	workflowName   string
+	workflowExecID string
+	workItemName   string
+	workItemExecID string
+	eventType      string    // work start / timeout / complete (assigned when event created) 
+	state          string    // running / pending / ... (changable)
+	payload        string    // metadata - 
+	createTime     time.Time 
+	completeTime   time.Time
+=======
+	EventID        string
+	WorkflowName   string
+	WorkflowExecID string
+	WorkItemName   string
+	EventType      string // work start / timeout / complete (immutable)
+	Payload        string
+	CreateTime     time.Time
+	CompleteTime   time.Time
+>>>>>>> c7abb6b91857149f6fd7a6d8731e2a15aaa8905f
+>>>>>>> Stashed changes
 }
 
 // GetPartitionKey returns the partition key for an event.
