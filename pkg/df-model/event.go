f66d45433018c6d1628f9cedce73d671b0287822
ducs1988 <ducs1988@gmail.com>
2018-02-26 00:20:19 -0600

    Add EventType, assign const to this type definiton

--- pkg/df-model/event.go
+++ pkg/df-model/event.go
@@ -5,13 +5,22 @@ import (
 	"time"
 )
 
+// EventType defines the type of current event ,this type will be assigned when event created
+type EventType string
+
+const (
+	start    EventType = "Start"
+	timeout  EventType = "Timeout"
+	complete EventType = "Complete"
+)
+
 // Event defines the model of a workflow event.
 type Event struct {
 	EventID        string
 	WorkflowName   string
 	WorkflowExecID string
 	WorkItemName   string
-	EventType      string // work start / timeout / complete (immutable)
+	EventType      EventType // work start / timeout / complete (immutable)
 	Payload        string
 	CreateTime     time.Time
 	CompleteTime   time.Time
